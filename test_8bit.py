d7ea5498a6ca3e9486b2f42e875b67e283022478
jhss <mtvfc@naver.com>
2025-01-29 12:11:07 +0000

    temp

--- test_8bit.py
+++ test_8bit.py
@@ -1,8 +1,9 @@
 import torch
 
-from modules import int_exp_shift
+from modules import *
 
 base_path = "/home/dsq_test"
+bit=8
 
 # 1. load input
 input_int = torch.load(f"{base_path}/data/qk_int.pt", weights_only=True)
@@ -12,15 +13,25 @@ input_scale = torch.tensor(input_scale)
 # 2. Calculate input_max_subtract scale
 input_float = input_int * input_scale
 input_float_max = torch.amax(input_float, dim=-1, keepdim=True)
+input_float_subtract = input_float - input_float_max
+input_subtract_scale = get_scale(input_float_subtract, bit)
+combined_scale = torch.round((input_subtract_scale / input_scale) * (2**bit)).to(torch.int16)
 
 # 2. Calculate integer-only exp
 input_int_max = torch.amax(input_int, dim=-1, keepdim=True)
-input_int_subtract = input_int.to(torch.int16) - input_int_max.to(torch.int16)
-exp_int, exp_scale = int_exp_shift(input_int_subtract, input_scale)
+input_int_subtract = input_int.to(torch.int64) - input_int_max.to(torch.int64)
+input_int_subtract_shift = (combined_scale * input_int_subtract) >> bit
+exp_int, exp_scale = int_exp_shift(input_int_subtract_shift, input_subtract_scale)
+
+# 2.1. Check input subtract shift
+input_int_subtract_quant = input_int_subtract_shift * input_subtract_scale
+print("=======================================================================================")
+print("[DEBUG] input_int_subract_quant: ", input_int_subtract_quant)
+print("[DEBUG] input_float_subtract: ", input_float_subtract)
+exit(0)
 
 # 3. Calculate torch float exp
-input_quant = input_int_subtract * input_scale
-exp_float = torch.exp(input_quant)
+exp_float = torch.exp(input_float_subtract)
 exp_quant = exp_int * exp_scale
 
 print("[DEBUG] exp_quant: ", exp_quant)
